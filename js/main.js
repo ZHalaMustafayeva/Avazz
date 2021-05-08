d4299a6eca5a448314a3e93fd48a173033b9bcc7
Zhala Mustafayeva <email@example.com>
2021-05-08 17:56:01 +0400

    sdkjfhds

--- js/main.js
+++ js/main.js
@@ -5,132 +5,141 @@
 
 $(document).ready(function () {
     $(window).bind('scroll', function () {
-        var navHeight = $('header').height();
+
+        var navHeight = $('header').height()
         if ($(window).scrollTop() > navHeight) {
-          $('header').addClass('fixed');
+            $('header').addClass('fixed');
+            $('.humb').css({
+                'background-color': 'black',
+                'border': '1px solid black'
+            })
         }
         else {
-          $('header').removeClass('fixed');
-    
+            $('header').css('background', 'transparent')
+            $('header').removeClass('fixed');
+            $('.humb').css({
+                'background-color': 'white',
+                'border': '1px solid white'
+            })
+
         }
-      });
+    });
 
     $('.az-flag').click(function (e) {
         e.stopPropagation();
         $('.flag-drop').toggleClass('active')
-        if($('.flag-drop').hasClass('active')){
+        if ($('.flag-drop').hasClass('active')) {
             $('.flag-drop').css({
-                'opacity':'1',
-                'transition':'.4s',
-                'pointer-events':'auto'
+                'opacity': '1',
+                'transition': '.4s',
+                'pointer-events': 'auto'
             })
         }
-        else{
+        else {
             $('.flag-drop').css({
-                'opacity':'0',
-                'transition':'.4s',
-                'pointer-events':'none'
-    
+                'opacity': '0',
+                'transition': '.4s',
+                'pointer-events': 'none'
+
             })
         }
-      });
-      $('body').click(function (e){
+    });
+    $('body').click(function (e) {
         $('.flag-drop').css({
-            'opacity':'0',
-            'transition':'.4s'
+            'opacity': '0',
+            'transition': '.4s'
         })
-      })
-    
-      var owl = $('.owl-carousel');
+    })
+
+    var owl = $('.owl-carousel');
     owl.owlCarousel({
-        loop:true,
-        nav:true,
-        margin:10,
-        dots:true,
-        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        nav: true,
+        margin: 10,
+        dots: true,
+        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
-            },            
-            960:{
-                items:2
+            600: {
+                items: 2
             },
-            1200:{
+            960: {
+                items: 2
+            },
+            1200: {
                 dotsEach: 5,
-                items:3
+                items: 3
             }
         },
-        
+
     });
 
-
-    // owl.on('mousewheel', '.owl-stage', function (e) {
-    //     if (e.deltaY>0) {
-    //         owl.trigger('next.owl');
-    //     } else {
-    //         owl.trigger('prev.owl');
-    //     }
-    //     e.preventDefault();
-    // });
- 
-    
-  
-    $('.hamburger-menu').click(function(){
+    $('.hamburger-menu').click(function () {
         $('.hamburger-menu').toggleClass('active-humb');
-        if($('.hamburger-menu').hasClass('active-humb')){
-            $('header').css('background','white')
-        $('.mobile-overlay').css({
-            transform:'translateX(0)',
-              transition:'.5s'
-          });;
+        if ($('.hamburger-menu').hasClass('active-humb')) {
+            $('header').css('background', 'white')
+            $('.mobile-overlay').css({
+                transition: '.5s',
+                'opacity':1,
+                'visibility': 'visible'
+            });;
             $('.mobile-menu').css({
-              transform:'translateX(0)',
-                transition:'.5s'
+                transform: 'translateX(0)',
+                transition: '.5s'
             });
             $('.humb').css({
-                'background-color':'black',
-                'border':'1px solid black'
+                'background-color': 'black',
+                'border': '1px solid black'
             })
         }
-        else{
+        else {
             $('.humb').css({
-                'background-color':'white',
-                'border':'1px solid white'
+                'background-color': 'black',
+                'border': '1px solid black'
             })
-            $('header').css('background','transparent')
+            $('header').css('background', 'transparent')
             $('.mobile-overlay').css({
-                transform:'translateX(200%)',
-                  transition:'.5s'
-              });
+                transition: '.4s',
+                'opacity':0,
+                'visibility': 'hidden'
+            });
             $('.mobile-menu').css({
-                transform:'translateX(200%)',
-                transition:'.5s'
-            });   
-            
+                transform: 'translateX(200%)',
+                transition: '.5s'
+            });
+
         }
     })
     $(".mobile-overlay").click(function () {
-        $('header').css('background','transparent')
-        $('.humb').css({
-            'background-color':'black',
-            'border':'1px solid black'
-        })
+        if ($('header').css('background', 'transparent') == true) {
+            $('.humb').css({
+                'background-color': 'white',
+                'border': '1px solid white'
+            })
+        }
+        else if ($('header').css('background', 'white') == true) {
+            $('.humb').css({
+                'background-color': 'black',
+                'border': '1px solid black'
+            })
+        }
+
+
         $('.hamburger-menu').removeClass('active-humb');
         $('.mobile-overlay').css({
-            transform:'translateX(200%)',
-              transition:'.5s'
-          });
+            transition: '.4s',
+                'opacity':0,
+                'visibility': 'hidden'
+        });
         $('.mobile-menu').css({
-            transform:'translateX(200%)',
-              transition:'.5s'
-          });
-          
-      });
+            transform: 'translateX(200%)',
+            transition: '.5s'
+        });
 
- 
-  })
+    });
 
- 
+
+})
+
